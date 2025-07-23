1cdfbd3e6de9a212371ba1bb52c23900bcbf2db4
yenna <yena.gwak@gmail.com>
2025-07-23 10:19:03 +0900

    [feature] TDD Cycle 3 - refactor

--- TDD_Prime/prime_factors.cpp
+++ TDD_Prime/prime_factors.cpp
@@ -5,7 +5,9 @@ class PrimeFactors {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {};
-		if (num > 1) result.push_back(num);
+		if (num > 1) {
+			result.push_back(num);
+		}
 		return result;
 	}
 };
