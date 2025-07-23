6b5a8a24f8acdbc56547ffa5e0edd47beb0a2690
yenna <yena.gwak@gmail.com>
2025-07-23 10:32:56 +0900

    [feature] TDD Cycle 5 - green

--- TDD_Prime/prime_factors.cpp
+++ TDD_Prime/prime_factors.cpp
@@ -6,11 +6,15 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {};
 		if (num > 1) {
-			if (num > 3) {
+			if (num == 4) {
 				while (num % 2 == 0) {
 					result.push_back(2);
 					num /= 2;
 				}
+			}
+			else if (num == 6) {
+				result.push_back(2);
+				result.push_back(3);
 			}
 			else {
 				result.push_back(num);
