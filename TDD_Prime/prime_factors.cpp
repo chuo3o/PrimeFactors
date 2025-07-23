200d326b6e81e1cf47b060e05c727562dd8e4eab
yenna <yena.gwak@gmail.com>
2025-07-23 10:40:39 +0900

    [feature] TDD Cycle 7 - refactor

--- TDD_Prime/prime_factors.cpp
+++ TDD_Prime/prime_factors.cpp
@@ -5,21 +5,14 @@ class PrimeFactors {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {};
-		if (num > 1) {
-			if (num == 4 || num == 6 || num == 9 || num == 12) {
-				for (int divisor = 2; num > 1; divisor++)
-				{
-					while (num % divisor == 0) {
-						result.push_back(divisor);
-						num /= divisor;
-					}
-				}
-			}
-			else {
-				result.push_back(num);
+		
+		for (int divisor = 2; num > 1; divisor++) {
+			while (num % divisor == 0) {
+				result.push_back(divisor);
+				num /= divisor;
 			}
 		}
-		
+
 		return result;
 	}
 };
